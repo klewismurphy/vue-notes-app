827c49264d929e7581a7a801cfb56e595169e86b
Kaitlin <klewismurphy@gmail.com>
2021-04-18 10:27:33 -0500

    adds components of toolbar and note slectors, selector, editor, but data is not yet dynamic

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="note-container">
+    <NoteSelector />
+    <NoteEditor />
+  </div>
+</template>
+
+<script>
+import NoteSelector from "./NoteSelector.vue";
+import NoteEditor from "./NoteEditor.vue";
+export default {
+  name: "notecontainer",
+  components: {
+    NoteSelector,
+    NoteEditor,
+  },
+};
+</script>
---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="note-selectors">
+    <NoteSelectors />
+    <NoteSelectors />
+    <NoteSelectors />
+  </div> 
+</template>
+
+<script>
+import NoteSelectors from "./NoteSelectors.vue";
+export default {
+  name: "noteselector",
+  components: {
+    NoteSelectors,
+  },
+};
+</script>
